eb9118b9da93f98dffde05a67ae1e1d0d3800600
smallwisp <4ubarov96@mail.ru>
2023-01-28 23:49:38 +0300

    added server request with fetch and get usd exchange rate

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
   const usdInput = document.querySelector('.usd'),
         rubInput = document.querySelector('.rub'),
-        reverseUsdInput = document.querySelector('.reverse-usd'),
+        reverseUSDInput = document.querySelector('.reverse-usd'),
         reverseRubInput = document.querySelector('.reverse-rub'),
         btn = document.querySelectorAll('.converter-run');
+
+  let rateUSD;
 
   const checkInput = (input) => {
     input.addEventListener('input', (event) => {
@@ -16,9 +18,9 @@ window.addEventListener('DOMContentLoaded', () => {
     let target = event.target;
     let inputParent = target.previousElementSibling.classList;
     if (Array.from(inputParent).includes('usd-rub')) {
-      rubInput.value = Math.ceil(usdInput.value / 30);
+      rubInput.value = +(usdInput.value * rateUSD).toFixed(2);
     } else {
-      reverseUsdInput.value = reverseRubInput.value * 30;
+      reverseUSDInput.value = +(reverseRubInput.value / rateUSD).toFixed(2);
     }
     console.log(Array.from(inputParent));
   };
@@ -30,6 +32,23 @@ window.addEventListener('DOMContentLoaded', () => {
   checkInput(usdInput);
   checkInput(rubInput);
 
+  let myHeaders = new Headers();
+    myHeaders.append("apikey", "j22BtkkANCXRbwRJXwFwCGdDAx2RFZ75");
+
+  let requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: myHeaders
+  };
+
+  
+  fetch("https://openexchangerates.org/api/latest.json?app_id=707ad2dc0afc4377999d8ca1b3dbddf2")
+  .then(response => response.json())
+  .then(result => {
+    rateUSD = result.rates.RUB;
+    console.log(result.rates.RUB)
+  })
+  .catch(error => console.log('error', error));
 
 
 })
