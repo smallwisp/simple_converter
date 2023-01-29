08829ec0c786619a981ed0eba28ffaa98058151a
smallwisp <4ubarov96@mail.ru>
2023-01-29 03:15:27 +0300

    completed tasks

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ window.addEventListener('DOMContentLoaded', () => {
         rubInput = document.querySelector('.rub'),
         reverseUSDInput = document.querySelector('.reverse-usd'),
         reverseRubInput = document.querySelector('.reverse-rub'),
-        btn = document.querySelectorAll('.converter-run');
+        btn = document.querySelectorAll('.converter-run'),
+        select = document.getElementById('select'),
+        divMoney = document.querySelectorAll('.money');
 
-  let rateUSD;
+  let rateUSD, rateEUR;
 
   const checkInput = (input) => {
     input.addEventListener('input', (event) => {
@@ -14,15 +16,42 @@ window.addEventListener('DOMContentLoaded', () => {
     })
   };
 
+  const chooseCurrency = () => {
+    select.addEventListener('change', (event) => {
+      let value = event.target.value;
+      if (value === 'EUR') {
+        divMoney.forEach(item => {
+          item.textContent = 'Евро(EUR)'
+        })
+      } else {
+        divMoney.forEach(item => {
+          item.textContent = 'Доллар США(USD)'
+        })
+      }
+      return value;
+    })
+  };
+
+  chooseCurrency();  
+  
   const convert = (event) => {
     let target = event.target;
     let inputParent = target.previousElementSibling.classList;
-    if (Array.from(inputParent).includes('usd-rub')) {
-      rubInput.value = +(usdInput.value * rateUSD).toFixed(2);
+    let rate;
+    let currency = select.value;
+
+    if (currency === 'USD') {
+      rate = rateUSD;
     } else {
-      reverseUSDInput.value = +(reverseRubInput.value / rateUSD).toFixed(2);
+      rate = rateEUR;
     }
-    console.log(Array.from(inputParent));
+    if (Array.from(inputParent).includes('usd-rub')) {
+      rubInput.value = +(usdInput.value * rate).toFixed(2);
+    } else {
+      reverseUSDInput.value = +(reverseRubInput.value / rate).toFixed(2);
+    }
+    console.log(rate);
+    console.log(currency);
   };
 
   btn.forEach(item => {
@@ -32,23 +61,17 @@ window.addEventListener('DOMContentLoaded', () => {
   checkInput(usdInput);
   checkInput(rubInput);
 
-  let myHeaders = new Headers();
-    myHeaders.append("apikey", "j22BtkkANCXRbwRJXwFwCGdDAx2RFZ75");
-
-  let requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-    headers: myHeaders
-  };
-
-  
   fetch("https://openexchangerates.org/api/latest.json?app_id=707ad2dc0afc4377999d8ca1b3dbddf2")
   .then(response => response.json())
   .then(result => {
     rateUSD = result.rates.RUB;
-    console.log(result.rates.RUB)
+    rateEUR = rateUSD /  result.rates.EUR;
+    console.log(rateUSD);
+    console.log(rateEUR);
   })
   .catch(error => console.log('error', error));
+
+
 
 
 })
